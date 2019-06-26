639e196047bfe7db60c73215cff4f20e9ee96a22
bholagabbar <dev.shreyans96@gmail.com>
2019-06-26 14:28:57 +0530

    Adding output log

--- charger.py
+++ charger.py
@@ -9,5 +9,9 @@ Notify.init("Charging Status")
 source = power.PowerManagement().get_providing_power_source_type()
 
 if source == power.POWER_TYPE_BATTERY:
+    print('NOT CHARGING!')
     notification = Notify.Notification.new('WARNING - Laptop running on battery!')
     notification.show()
+else:
+    print('CHARGING')
+
